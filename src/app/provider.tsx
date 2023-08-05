5a96ecb7b0b548d384222b364b03b542cbe0a429
Xuan Vu <xuanbvu5@gmail.com>
2023-08-04 23:21:16 -0700

    next auth credentials set up

---
+++ src/app/provider.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { SessionProvider } from 'next-auth/react'
+
+type ProviderProps = {
+  children?: React.ReactNode
+}
+
+export default function Provider({ children }: ProviderProps) {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  )
+}
